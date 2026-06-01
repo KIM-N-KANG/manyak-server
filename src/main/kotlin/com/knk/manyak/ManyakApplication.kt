4296dbac55b35b96e17e4d6101a426e76c7e21de
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2026-06-01 19:48:36 +0900

    Init: 프로젝트 초기화

---
+++ src/main/kotlin/com/knk/manyak/ManyakApplication.kt
@@ -0,0 +1,11 @@
+package com.knk.manyak
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ManyakApplication
+
+fun main(args: Array<String>) {
+    runApplication<ManyakApplication>(*args)
+}
---
+++ src/test/kotlin/com/knk/manyak/ManyakApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.knk.manyak
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ManyakApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
